664fa54010d012ea076af30f0f9910c0442d4cbf
ljh <59722639+ywsygdys119@users.noreply.github.com>
2021-04-20 14:52:06 +0800

    update
    ljh

--- src/main/java/com/bayee/political/domain/RiskConductVisitRecord.java
+++ src/main/java/com/bayee/political/domain/RiskConductVisitRecord.java
@@ -20,6 +20,8 @@ public class RiskConductVisitRecord {
     private Date creationDate;
 
     private Date updateDate;
+    
+    private String typeName;
 
     public Integer getId() {
         return id;
@@ -92,6 +94,13 @@ public class RiskConductVisitRecord {
 	public void setDeductionScore(Double deductionScore) {
 		this.deductionScore = deductionScore;
 	}
-    
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
     
 }

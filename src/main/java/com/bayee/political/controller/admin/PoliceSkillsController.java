314ef6c83a613eb7b2b5606a990d6cc54019853e
zouya <2290632644@qq.com>
2021-08-02 17:22:46 +0800

    提交警务技能controller

---
+++ src/main/java/com/bayee/political/controller/admin/PoliceSkillsController.java
@@ -0,0 +1,11 @@
+package com.bayee.political.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zouya
+ */
+@RestController
+public class PoliceSkillsController {
+
+}
